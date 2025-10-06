df1d1c7df9a9ecc411488af529567f1a241ea4af
Lukas Simon <lukas@flecs.tech>
2025-10-06 15:25:21 +0200

    backend: Check and set super admin on routes GET/POST /users/super-admin

--- app/backend/src/model/user.rs
+++ app/backend/src/model/user.rs
@@ -5,10 +5,32 @@ use crate::model::password;
 
 pub type Uid = u16;
 
+pub const SUPER_ADMIN_ID: u16 = 0;
+
 #[derive(Serialize, Deserialize, ToSchema)]
 pub struct User {
     pub uid: Uid,
     pub name: String,
     pub full_name: String,
     pub password: password::Password,
+}
+
+#[derive(Serialize, Deserialize, ToSchema)]
+pub struct SuperAdmin {
+    pub name: String,
+    pub full_name: String,
+    pub password: String,
+}
+
+impl TryFrom<SuperAdmin> for User {
+    type Error = anyhow::Error;
+
+    fn try_from(value: SuperAdmin) -> Result<Self, Self::Error> {
+        Ok(Self {
+            uid: SUPER_ADMIN_ID,
+            name: value.name,
+            full_name: value.full_name,
+            password: password::Password::new(&value.password)?,
+        })
+    }
 }
--- app/backend/src/persist/user_db.rs
+++ app/backend/src/persist/user_db.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use crate::model::user::{Uid, User};
+use crate::model::user::{SUPER_ADMIN_ID, SuperAdmin, Uid, User};
 
 pub struct UserDB {
     path: PathBuf,
@@ -16,11 +16,37 @@ impl UserDB {
     pub fn query_by_name(&self, name: &str) -> Option<&User> {
         self.users.iter().find(|u| u.name == name)
     }
+
+    pub fn save(&self) -> anyhow::Result<()> {
+        super::save_to_file(&self.path, &self.users)
+    }
+
+    pub fn query_by_uid(&self, uid: Uid) -> Option<&User> {
+        self.users.iter().find(|u| u.uid == uid)
+    }
+
+    pub fn contains_super_admin(&self) -> bool {
+        self.query_by_uid(SUPER_ADMIN_ID).is_some()
+    }
+
+    pub fn set_super_admin(&mut self, super_admin: SuperAdmin) -> anyhow::Result<Option<User>> {
+        let super_admin = super_admin.try_into()?;
+        if let Some(existing_admin) = self
+            .users
+            .iter_mut()
+            .find(|user| user.uid == SUPER_ADMIN_ID)
+        {
+            Ok(Some(std::mem::replace(existing_admin, super_admin)))
+        } else {
+            self.users.push(super_admin);
+            Ok(None)
+        }
+    }
 }
 
 impl Drop for UserDB {
     fn drop(&mut self) {
-        super::save_to_file(&self.path, &self.users)
+        self.save()
             .unwrap_or_else(|e| println!("{}", format!("Could not persist user database: {}", e)));
     }
 }
