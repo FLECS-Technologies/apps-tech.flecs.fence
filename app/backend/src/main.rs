e995ef82929d4a37c1bd1589989f5af265870ef2
Lukas Simon <lukas@flecs.tech>
2026-03-03 12:36:28 +0100

    Remove unused code

--- app/backend/src/main.rs
+++ app/backend/src/main.rs
@@ -25,30 +25,4 @@ async fn main() {
         .with_graceful_shutdown(signal_handler())
         .await
         .unwrap();
-    // let client_registry = client_registry();
-    // let authorizer = AuthMap::new(RandomGenerator::new(16));
-    // let solicitor = FnSolicitor(|_req| Ok(())); // Simplified user consent
-    // let mut oauth = Endpoint::new(client_registry, authorizer, solicitor);
-    // match oauth.authorize(req.into_inner()).await {
-    //     Ok(response) => Ok(response.into()),
-    //     Err(err) => Err(err.into()),
-    // }
 }
-
-// async fn token(
-//     mut req: OAuthRequest<WebRequest>,
-// ) -> Result<OAuthResponse<WebResponse>, OAuthFailure> {
-//     let client_registry = client_registry();
-//     let authorizer = AuthMap::new(RandomGenerator::new(16));
-//     let issuer = TokenMap::new(RandomGenerator::new(16));
-//     let mut oauth = Endpoint::new(client_registry, authorizer, issuer);
-//     match oauth.token(req.into_inner()).await {
-//         Ok(response) => Ok(response.into()),
-//         Err(err) => Err(err.into()),
-//     }
-// }
-//
-// async fn authorize(
-//     mut req: OAuthRequest<WebRequest>,
-// ) -> Result<OAuthResponse<WebResponse>, OAuthFailure> {
-// }
